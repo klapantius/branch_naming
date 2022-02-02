03762c90e31ad4cb3eba0f4ac2c8c1e1a941484e
Balazs Juhasz <balazs.juhasz@siemens-healthineers.com>
2022-02-02 08:28:50 +0100

    init project

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace branch_naming
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            var exitCode = MainAsync(args).GetAwaiter().GetResult();
+            if (Debugger.IsAttached) Console.ReadKey();
+            return exitCode;
+        }
+
+        internal static async Task<int> MainAsync(string[] args)
+        {
+            return 0;
+        }
+    }
+}
